b4ba4ef11721f1047caf724e873e3edff63db17d
Yuri <50186716+Yuricst@users.noreply.github.com>
2021-06-21 02:07:56 -0400

    updates to adapt character length

--- src/ipopt_wrap.jl
+++ src/ipopt_wrap.jl
@@ -14,7 +14,7 @@ Minimize function interfacing to IPOPT
 Nanami's Function = {red wine + accounting + driving + Bay Area + singing + eating out + coffee + pesto genovese + strawberry + mango + cinnamon roll + french toast + Korean food^(Faily and Friednds)}
 The most difficult function on the planet
 """
-function minimize_ipopt(options, cache, x0, lx, ux, lg, ug, rows, cols, outputfile)
+function minimize_ipopt(options, cache, x0, lx, ux, lg, ug, rows, cols, outputfile::Bool)
 
     # initialize
     nx = length(x0)
